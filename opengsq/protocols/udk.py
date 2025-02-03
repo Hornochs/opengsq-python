72cbc5664814b20a5135e7181697830b2f356300
Hornochs <chaos_bloated856@memehorst.de>
2025-02-03 16:26:05 +0100

    Adding Response Parsing

--- opengsq/protocols/udk.py
+++ opengsq/protocols/udk.py
@@ -57,5 +57,40 @@ class UDK(ProtocolBase):
 
     def _parse_response(self, buffer: bytes) -> dict:
         br = BinaryReader(buffer[self.LAN_BEACON_PACKET_HEADER_SIZE:])
-        # Implement parsing logic here
-        pass
+        
+        raw = {}
+        raw['name'] = br.read_string()
+        raw['map'] = br.read_string()
+        raw['game_type'] = br.read_string()
+        raw['num_players'] = br.read_int32()
+        raw['max_players'] = br.read_int32()
+        raw['password_protected'] = br.read_boolean()
+        raw['stats_enabled'] = br.read_boolean()
+        raw['lan_mode'] = br.read_boolean()
+        
+        players = []
+        while br.remaining() > 0:
+            player_name = br.read_string()
+            if not player_name:
+                break
+            players.append(Player(
+                name=player_name,
+                score=br.read_int32(),
+                ping=br.read_int32(),
+                team=br.read_int32()
+            ))
+        
+        raw['players'] = players
+        
+        return {
+            'name': raw['name'],
+            'map': raw['map'],
+            'game_type': raw['game_type'],
+            'num_players': raw['num_players'],
+            'max_players': raw['max_players'],
+            'password_protected': raw['password_protected'],
+            'stats_enabled': raw['stats_enabled'],
+            'lan_mode': raw['lan_mode'],
+            'players': players,
+            'raw': raw
+        }
