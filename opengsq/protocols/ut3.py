e332f9186a0d3d473e978ce4ff55df336c430054
Hornochs <chaos_bloated856@memehorst.de>
2025-02-04 11:06:13 +0100

    Adding Gamemode Translation

--- opengsq/protocols/ut3.py
+++ opengsq/protocols/ut3.py
@@ -2,6 +2,18 @@ from opengsq.protocols.udk import UDK
 from opengsq.responses.ut3.status import Status
 
 class UT3(UDK):
+    GAMEMODE_NAMES = {
+        0: "Deathmatch",
+        1: "Team Deathmatch", 
+        2: "Capture The Flag",
+        3: "Vehicle CTF",
+        4: "Warfare",
+        5: "Duel",
+        6: "Campaign",
+        7: "Greed",
+        8: "Betrayal",
+        9: "Custom"
+    }
     full_name = "Unreal Tournament 3 Protocol"
     
     def __init__(self, host: str, port: int = 14001, timeout: float = 5.0):
@@ -11,23 +23,23 @@ class UT3(UDK):
     def _parse_response(self, buffer: bytes) -> dict:
         base_response = super()._parse_response(buffer)
         
-        # Process UT3 specific properties
+        # Process properties
         ut3_properties = {}
         for prop in base_response['raw']['settings_properties']:
             prop_id = prop['id']
-            if prop_id == 1073741825:  # Map
+            if prop_id == 1073741825:      # Map
                 base_response['map'] = prop['data']
                 ut3_properties['map'] = prop['data']
-            elif prop_id == 1073741826:  # Game Type
+            elif prop_id == 1073741826:     # Game Type
                 base_response['game_type'] = prop['data']
                 ut3_properties['gametype'] = prop['data']
-            elif prop_id == 1073741828:  # Custom Mutators
+            elif prop_id == 1073741828:     # Custom Mutators
                 ut3_properties['custom_mutators'] = self._split_mutators(prop['data'])
-            elif prop_id == 268435704:  # Frag Limit
+            elif prop_id == 268435704:      # Frag Limit
                 ut3_properties['frag_limit'] = prop['data']
-            elif prop_id == 268435705:  # Time Limit
+            elif prop_id == 268435705:      # Time Limit
                 ut3_properties['time_limit'] = prop['data']
-            elif prop_id == 268435703:  # Number of Bots
+            elif prop_id == 268435703:      # Number of Bots
                 ut3_properties['numbots'] = prop['data']
             elif prop_id == 268435717 or prop_id == 1073741829:  # Stock Mutators
                 ut3_properties['stock_mutators'] = self._split_mutators(prop['data'])
@@ -35,15 +47,21 @@ class UT3(UDK):
         # Process localized settings
         for setting in base_response['raw']['localized_settings']:
             setting_id = setting['id']
-            if setting_id == 32779:
-                ut3_properties['gamemode'] = setting['value_index']
+            value_index = setting['value_index']
+            
+            if setting_id == 32779:  # Game Mode
+                ut3_properties['gamemode'] = self.GAMEMODE_NAMES.get(value_index, f"Unknown_{value_index}")
             elif setting_id == 0:
-                ut3_properties['bot_skill'] = setting['value_index']
+                ut3_properties['bot_skill'] = value_index
             elif setting_id == 6:
-                ut3_properties['pure_server'] = setting['value_index']
+                ut3_properties['pure_server'] = value_index
             elif setting_id == 7:
-                base_response['password_protected'] = setting['value_index'] == 1
-                ut3_properties['password'] = setting['value_index']
+                base_response['password_protected'] = value_index == 1
+                ut3_properties['password'] = value_index
+            elif setting_id == 8:
+                ut3_properties['vs_bots'] = value_index
+            elif setting_id == 10:
+                ut3_properties['force_respawn'] = value_index
 
         base_response['raw'].update(ut3_properties)
         return base_response
